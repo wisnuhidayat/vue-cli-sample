622e6ddcf46fd4dca30e1601b1eba2169d447338
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-18 12:47:30 +0000

    Deploy wisnuhidayat/vue-cli-sample to github.com/wisnuhidayat/vue-cli-sample.git:gh-pages

---
+++ precache-manifest.2d91de77199346f4b8aeaebc063a5ff4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b760e0bb789e8856001e",
+    "url": "/css/page-films.1692b3d8.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b760e0bb789e8856001e",
+    "url": "/js/page-films.a7e70740.js"
+  },
+  {
+    "revision": "a7288df2e40c012e9d85",
+    "url": "/js/page-film-detail.91f0924e.js"
+  },
+  {
+    "revision": "ffa708826eb570c00fcc",
+    "url": "/js/page-about.cde921c5.js"
+  },
+  {
+    "revision": "a284243652fb16d3db57",
+    "url": "/js/chunk-vendors.3187719c.js"
+  },
+  {
+    "revision": "f3d54335097161bc5bff",
+    "url": "/js/app.06395a48.js"
+  },
+  {
+    "revision": "d580b92bd73435fda311d4e4f5953f05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f3d54335097161bc5bff",
+    "url": "/css/app.1100ae40.css"
+  },
+  {
+    "revision": "6a02faf7ea2a9584134ffe15779a0e44",
+    "url": "/_redirects"
+  }
+];
