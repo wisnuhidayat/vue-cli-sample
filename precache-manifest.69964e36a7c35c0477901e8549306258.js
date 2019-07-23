a634b7715eac1fded71caef8613cd70d8abe4fca
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-23 11:28:35 +0000

    Deploy wisnuhidayat/vue-cli-sample to github.com/wisnuhidayat/vue-cli-sample.git:gh-pages

--- precache-manifest.69964e36a7c35c0477901e8549306258.js
+++ precache-manifest.69964e36a7c35c0477901e8549306258.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = [
   {
-    "revision": "567e01b717575c027507",
-    "url": "/js/page-films.c52904dd.js"
+    "revision": "c172e830aff0eba4be13",
+    "url": "/js/page-people.0f250a4e.js"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
     "url": "/robots.txt"
   },
   {
-    "revision": "82b9c7a5a3f405032b1db71a25f67021",
-    "url": "/img/logo.82b9c7a5.png"
+    "revision": "a4fc652bda8f7d04925a",
+    "url": "/js/page-people-detail.f5309df9.js"
   },
   {
     "revision": "c23b07c727d650ff2950",
@@ -20,23 +20,35 @@ self.__precacheManifest = [
     "url": "/js/page-about.4ced9594.js"
   },
   {
-    "revision": "224827de055044fe4b13",
-    "url": "/js/page-film-detail.1c29e0b0.js"
+    "revision": "e8ea2be7d17afbe5b2d4",
+    "url": "/js/page-film-detail.ad07733e.js"
   },
   {
-    "revision": "567e01b717575c027507",
+    "revision": "d77f43bf2be420cd5dc5",
     "url": "/css/page-films.1692b3d8.css"
   },
   {
-    "revision": "029c3741d9eb22884666",
-    "url": "/js/app.5a8a36c2.js"
+    "revision": "d77f43bf2be420cd5dc5",
+    "url": "/js/page-films.355ffc9e.js"
+  },
+  {
+    "revision": "c172e830aff0eba4be13",
+    "url": "/css/page-people.6a6040f0.css"
+  },
+  {
+    "revision": "939af80045dce25fbe38",
+    "url": "/js/app.7b3ebb7a.js"
   },
   {
     "revision": "c23b07c727d650ff2950",
     "url": "/js/chunk-vendors.5fb86764.js"
   },
   {
-    "revision": "4581180e0f4c73d905b356e72725f7e4",
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "cc47aa7c54d8325889ba0d57135af24c",
     "url": "/index.html"
   },
   {
@@ -52,8 +64,8 @@ self.__precacheManifest = [
     "url": "/index.html.html"
   },
   {
-    "revision": "029c3741d9eb22884666",
-    "url": "/css/app.1100ae40.css"
+    "revision": "939af80045dce25fbe38",
+    "url": "/css/app.4163dd68.css"
   },
   {
     "revision": "6a02faf7ea2a9584134ffe15779a0e44",
